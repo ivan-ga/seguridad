33bed40e152ecfa3d3cb9c1c6fde68ae32cf4546
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-02-07 19:03:31 +0000

    mas cosas

--- p1/vernam.rb
+++ p1/vernam.rb
@@ -7,12 +7,15 @@ CLAVE = 0b001111000001100001110011 #clave para el cifrado
 class Vernam
   attr_accessor :mensaje
   attr_accessor :mensaje_original_binario
+  attr_accessor :mensaje_cifrado_binario
+  attr_accessor :mensaje_cifrado
 
   #constructor
   def initialize(m)
     @mensaje = m
     @mensaje_original_binario = nil
     @mensaje_cifrado = nil
+    @mensaje_cifrado_binario = nil
   end
 
   def cifrar
@@ -23,8 +26,11 @@ class Vernam
     end
 
     c=Integer(a.join(''))
+    @mensaje_original_binario = c
     resultado = c ^ CLAVE
-    @mensaje_original_binario = resultado.to_s(2)
+    @mensaje_cifrado_binario = '0' + resultado.to_s(2)
+
+    @mensaje_cifrado = a.join('').unpack('C*')
   end
 
 
@@ -36,7 +42,14 @@ m1 = gets #almacena en la variable m1 el texto introducido por el usuario
 m1.delete!("\n") #elimino el salto de línea que se incluye por defecto
 
 mensaje_a_cifrar = Vernam.new(m1)
+mensaje_a_cifrar.cifrar()
 
 puts "Mensaje original: #{m1}"
 
-puts "Mensaje original en binario: #{mensaje_a_cifrar.cifrar()}"
+puts "Mensaje original en binario: #{mensaje_a_cifrar.mensaje_original_binario.to_s(2)}"
+
+puts "Clave aleatoria: #{CLAVE.to_s(2)}"
+
+puts "Mensaje cifrado en binario: #{mensaje_a_cifrar.mensaje_cifrado_binario}"
+
+puts mensaje_a_cifrar.mensaje_cifrado
