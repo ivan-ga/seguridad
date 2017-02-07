6d963bccb780ac30476dad6797dbf19303319aed
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-02-07 11:49:30 +0000

    todavia no funciona

--- p1/vernam.rb
+++ p1/vernam.rb
@@ -2,29 +2,30 @@
 #Autor: Iván González Aguiar
 #alu0100551266@ull.edu.es
 
-CLAVE = 0b001111000001100001110011 #clave para el cifrado
+CLAVE = "001111000001100001110011" #clave para el cifrado
 
 class Vernam
   attr_accessor :mensaje
 
+  #constructor
   def initialize(m)
-    @mensaje = m
+    @mensaje = m.unpack("B*") #convierte el mensaje m a binario con el método unpack
+    @mensaje_cifrado = nil
   end
 
-  def pasar_a_ascii
-    a = []
-    @mensaje.each_byte do |num|
-      a << num
-    end
-    a
+  def cifrar
+    @mensaje_cifrado = "0b + #{@mensaje[0]}" ^ CLAVE.to_i
   end
+
+
 
 end
-
+puts CLAVE
 print "Inserte mensaje: "
 m1 = gets #almacena en la variable m1 el texto introducido por el usuario
 m1.delete!("\n") #elimino el salto de línea que se incluye por defecto
 
 mensaje_a_cifrar = Vernam.new(m1)
-
-puts mensaje_a_cifrar.pasar_a_ascii()
+#puts mensaje_a_cifrar.mensaje[0].to_i(2)
+#mensaje_a_cifrar.cifrar()
+puts mensaje_a_cifrar.cifrar()
