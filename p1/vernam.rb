6dfa89ff1c05a3ecc79ff5111a38bc11bb76f5a9
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-02-07 18:33:45 +0000

    cifrado funcionando

--- p1/vernam.rb
+++ p1/vernam.rb
@@ -2,30 +2,41 @@
 #Autor: Iván González Aguiar
 #alu0100551266@ull.edu.es
 
-CLAVE = "001111000001100001110011" #clave para el cifrado
+CLAVE = 0b001111000001100001110011 #clave para el cifrado
 
 class Vernam
   attr_accessor :mensaje
+  attr_accessor :mensaje_original_binario
 
   #constructor
   def initialize(m)
-    @mensaje = m.unpack("B*") #convierte el mensaje m a binario con el método unpack
+    @mensaje = m
+    @mensaje_original_binario = nil
     @mensaje_cifrado = nil
   end
 
   def cifrar
-    @mensaje_cifrado = "0b + #{@mensaje[0]}" ^ CLAVE.to_i
+    a=['0b']
+
+    @mensaje.each_byte do |c|
+        a << '0' + c.to_s(2)
+    end
+
+    c=Integer(a.join(''))
+    resultado = c ^ CLAVE
+    @mensaje_original_binario = resultado.to_s(2)
   end
 
 
 
 end
-puts CLAVE
+
 print "Inserte mensaje: "
 m1 = gets #almacena en la variable m1 el texto introducido por el usuario
 m1.delete!("\n") #elimino el salto de línea que se incluye por defecto
 
 mensaje_a_cifrar = Vernam.new(m1)
-#puts mensaje_a_cifrar.mensaje[0].to_i(2)
-#mensaje_a_cifrar.cifrar()
-puts mensaje_a_cifrar.cifrar()
+
+puts "Mensaje original: #{m1}"
+
+puts "Mensaje original en binario: #{mensaje_a_cifrar.cifrar()}"
