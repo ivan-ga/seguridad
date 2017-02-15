e1074e70cb798f74a04625dcbf3b820df224b25d
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-02-15 11:03:33 +0000

    con modificación

--- p1/vernam.rb
+++ p1/vernam.rb
@@ -2,10 +2,12 @@
 #Autor: Iván González Aguiar
 #alu0100551266@ull.edu.es
 
-CLAVE = 0b001111000001100001110011 #clave para el cifrado
+# encoding: UTF-8
+#CLAVE = 0b001111000001100001110011 #clave para el cifrado
 #CLAVE = 0b0000111100100001 #clave para el segundo ejemplo
 #CLAVE =  0b00111100000110000111001100000000
 #CLAVE = 0b01010101
+CLAVE = 'SEGURO'
 
 class Vernam
   attr_accessor :mensaje
@@ -21,16 +23,32 @@ class Vernam
     @mensaje_cifrado_binario = nil
   end
 
-  def cifrar
+  def cifrar(n)
     a=['0b'] #tengo que añadir el 0b para que tome el número como binario
 
     @mensaje.each_byte do |c|
         a << '0' + c.to_s(2) #añado el 0 ya que lo simplifica por defecto
     end
 
+    cl=['0b']#array para almacenar los valores de la clave en binario
+
+    #pasar a binario la clave (modificación)
+    CLAVE.each_byte do |c|
+        cl << '0' + c.to_s(2) #añado el 0 ya que lo simplifica por defecto
+    end
+
+    cla=Integer(cl.join(''))
     c=Integer(a.join(''))
     @mensaje_original_binario = c
-    resultado = c ^ CLAVE
+
+    #hacer xor las veces que se indique (modificación)
+    iter = 0
+    resultado = c
+    while iter<n do
+      resultado = resultado ^ cla
+      iter = iter+1
+    end
+
     @mensaje_cifrado_binario = '0' + resultado.to_s(2)
 
     y=[]
@@ -45,13 +63,14 @@ class Vernam
     #convierto cada byte a su equivalente en ASCII y lo almaceno en el array mc
     mc=[]
     y.each do |x|
+      puts x
       mc << Integer(x).chr
     end
 
     @mensaje_cifrado = mc.join('')
   end
 
-  def descifrar
+  def descifrar(n)
     a=['0b'] #tengo que añadir el 0b para que tome el número como binario
 
     @mensaje.each_byte do |c|
@@ -64,10 +83,24 @@ class Vernam
       end
     end
 
+    #pasar a binario la clave (modificación)
+    cl=['0b'] #array para almacenar los valores de la clave en binario
+    CLAVE.each_byte do |c|
+        cl << '0' + c.to_s(2) #añado el 0 ya que lo simplifica por defecto
+    end
+
+    cla=Integer(cl.join(''))
     resultado=Integer(a.join(''))
     @mensaje_cifrado_binario = resultado
 
-    c = resultado ^ CLAVE
+    #hacer xor las veces que se indique (modificación)
+    iter = 0
+    c = resultado
+    while iter<n do
+      c = c ^ cla
+      iter = iter+1
+    end
+
     @mensaje_original_binario = '0' + c.to_s(2)
 
     y=[]
@@ -96,6 +129,11 @@ print "Inserte mensaje: "
 m1 = gets #almacena en la variable m1 el texto introducido por el usuario
 m1.delete!("\n") #elimino el salto de línea que se incluye por defecto
 puts ""
+print "Número de veces que se aplica XOR: "
+num_veces = gets #almacena en la variable m1 el texto introducido por el usuario
+num_veces.delete!("\n") #elimino el salto de línea que se incluye por defecto
+n_veces = num_veces.to_i #paso a entero el número de veces que se pidió por pantalla ya que si no se queda como string
+puts ""
 m = Vernam.new(m1)
 
 puts "MENU"
@@ -108,24 +146,24 @@ puts "Elige una opción
 case gets.strip
   when "1"
     puts ""
-    m.cifrar()
+    m.cifrar(n_veces)
 
     puts "Mensaje original: #{m.mensaje}"
 
     puts "Mensaje original en binario: #{m.mensaje_original_binario.to_s(2)}"
 
-    puts "Clave aleatoria: #{CLAVE.to_s(2)}"
+    #puts "Clave aleatoria: #{CLAVE.to_s(2)}"
 
     puts "Mensaje cifrado en binario: #{m.mensaje_cifrado_binario}"
 
     puts "Mensaje cifrado: #{m.mensaje_cifrado}"
   when "2"
     puts ""
-    m.descifrar()
+    m.descifrar(n_veces)
 
     puts "Mensaje cifrado: #{m.mensaje}"
 
-    puts "Mensaje cifrado en binario: #{m.mensaje_cifrado_binario}"
+    puts "Mensaje cifrado en binario: #{m.mensaje_cifrado_binario.to_s(2)}"
 
     puts "Mensaje descifrado en binario: #{m.mensaje_original_binario}"
 
