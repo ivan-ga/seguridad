8e891e1603ef382ab08245f0d76432951b1d89f4
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-04-27 19:54:56 +0100

    mas

--- p7/fiat-shamir.rb
+++ p7/fiat-shamir.rb
@@ -1,4 +1,7 @@
-
+# PRÁCTICA 7: ALGORITMO DE FIAT-SHAMIR
+# Autor: Iván González Aguiar
+# alu0100551266@ull.edu.es
+################################################################################
 
 #Algoritmo de Euclides para comprobar si dos números son primos entre sí
 def euclides(a, b)
@@ -26,3 +29,78 @@ def euclides(a, b)
   end
 
 end
+
+#Algoritmo de exponenciación rápida
+def exponenciacion(base,b,modulo) #b es el exponente
+      x = 1 #Inicialización de la x a 1
+      y = base % modulo #la y es igual a la base
+
+      while (b > 0) and (y>1) do
+          if ((b % 2)!=0) #si b es impar
+              x = (x*y) % modulo
+              b = b-1
+          else #si b es par
+              y = (y*y) % modulo
+              b = b/2
+          end
+      end
+
+      x
+  end
+
+##############################################################################3#
+
+class Probador #el A del enunciado
+
+  def initialize(s, x)
+    @a = nil
+    @y = nil
+    @s = s
+    @x = x
+  end
+
+
+
+end
+
+################################################################################
+
+class Verificador #el B del enunciado
+
+
+end
+
+
+
+################################################################################
+
+
+puts ""
+print "Inserte valor de p: "
+pe = gets.to_i
+puts " "
+
+if pe.prime? == false
+  while !pe.prime
+    puts ""
+    puts "El valor insertado no es primo, insertalo de nuevo."
+    print "Inserte valor de p: "
+    pe = gets.to_i
+    puts " "
+  end
+end
+
+print "Inserte valor de q: "
+q = gets.to_i
+puts " "
+
+print "Inserte valor de s: "
+s = gets.to_i
+puts " "
+
+print "Inserte número de iteraciones: "
+i = gets.to_i
+puts " "
+
+#Cálculo de la N
+n = pe * q
