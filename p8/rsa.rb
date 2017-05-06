353db100e237bde9ac6c30e9e31bfd411fa935b0
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-05-06 18:31:30 +0100

    funcionando

--- p8/rsa.rb
+++ p8/rsa.rb
@@ -153,19 +153,14 @@ def euclides(a, b)
 end
 
 #Algoritmo de Euclides extendido, para calcular el inverso de un número modulo con otro
-def euclides_extendido(a, b)
+def euclides_extendido(num, mod)
 
   z = nil
   anterior = 1
   ante_anterior = 0
 
-  if(a < b)
-    dividendo = b
-    divisor = a
-  else
-    dividendo = a
-    divisor = b
-  end
+  dividendo = mod
+  divisor = num
 
   resto = 1
 
@@ -188,22 +183,209 @@ def euclides_extendido(a, b)
 
 end
 
-#Método para calcular j-1, tamaño de bloques de texto
-def calcular_j(n)
-  j = 0
-  resultado = 0
 
-  while resultado<n
-    resultado = n ** j
-    j = j + 1
+class Rsa
+
+  #Setters y getters
+  attr_accessor :mensaje_original
+  attr_accessor :mensaje_codificado
+  attr_accessor :mensaje_cifrado
+  attr_accessor :pe
+  attr_accessor :q
+  attr_accessor :fi
+
+  #Constructor
+  def initialize(mensaje)
+    @mensaje_original = mensaje
+    @mensaje_codificado = nil
+    @mensaje_cifrado = nil
+    @pe = nil
+    @q = nil
+    @fi = nil
+    @d = nil
   end
 
-  j
+  #Método para calcular j-1, tamaño de bloques de texto
+  def calcular_j(n)
+    j = 0
+    resultado = 0
+
+    while resultado<n
+      resultado = TAM_ALFABETO ** j
+      if resultado<n
+        j = j + 1
+      end
+    end
+
+    j-1
+  end
+  #Método para la codificación del texto
+  def codificar(n)
+
+    j = calcular_j(n)
+
+    texto = @mensaje_original
+    texto = texto.delete!(' ') #elimino espacios
+    texto_bloques = texto.chars.each_slice(j).map(&:join) #array con el mensaje dividido en bloques de tamaño j-1
+    i = j - 1
+    resultado_parcial = 0
+    resultado = []
+
+    texto_bloques.each do |c|
+      i = j - 1
+      resultado_parcial = 0
+
+      c.split("").each do |k|
+        resultado_parcial = resultado_parcial + (ALFABETO[k]*(TAM_ALFABETO**i))
+        i = i-1
+      end
+
+      resultado << resultado_parcial
+    end
+
+    @mensaje_codificado = resultado
+
+  end
+
+  def calcular_fi(p, q)
+    @fi = (@pe-1)*(@q-1)
+  end
+
+  def cifrar(n, mensaje_en_bloques, e)
+    resultado = []
+    mensaje_en_bloques.each do |val|
+      resultado << exponenciacion(val, e, n)
+    end
+
+    @mensaje_cifrado = resultado
+  end
+
+  def descifrar(n, mensaje_en_bloques, e)
+    resultado = []
+    mensaje_en_bloques.each do |val|
+      resultado << exponenciacion(val, e, n)
+    end
+
+    @mensaje_cifrado = resultado #es el mensaje original
+  end
+
+
 end
 
-#Método para la codificación del texto
-def codificar(texto, n)
-  j = nil #el j-1 del enunciado
+
+################################################################################
+
+puts ""
+print "Inserte mensaje: "
+m1 = gets #almacena en la variable m1 el texto introducido por el usuario
+
+#se le da el formato adecuado al mensaje introducido por el usuario
+m1.delete!("\n") #elimino el salto de línea que se incluye por defecto
+#m1.delete!(' ') #elimino los espacios
+m1 = m1.upcase #se pasa el mensaje a mayúsculas
+
+puts ""
+print "Inserte valor de p: "
+pe = gets.to_i
+puts " "
+
+#si el valor no es primo, se vuelve a pedir
+if test_primalidad(pe) == false
+  while !test_primalidad(pe)
+    puts ""
+    puts "El valor insertado no es primo, insertalo de nuevo."
+    print "Inserte valor de p: "
+    pe = gets.to_i
+    puts " "
+  end
+end
+
+print "Inserte valor de q: "
+q = gets.to_i
+puts " "
+
+#Cálculo de la N
+n = pe * q
+
+#si el valor no es primo, se vuelve a pedir
+if test_primalidad(q) == false
+  while !test_primalidad(q)
+    puts ""
+    puts "El valor insertado no es primo, insertalo de nuevo."
+    print "Inserte valor de p: "
+    q = gets.to_i
+    puts " "
+  end
+end
+
+print "Inserte valor de d: "
+d = gets.to_i
+puts " "
+
+#si el valor es menor que cero o no es primo con N, se vuelve a pedir
+if d<0 || euclides(d, n) == false
+  while d<0 || euclides(d, n) == false
+    puts ""
+    puts "El valor insertado no es correcto, insertalo de nuevo."
+    print "Inserte valor de d: "
+    d = gets.to_i
+    puts " "
+  end
+end
+
+# print "Inserte valor de Φ: "
+# fi = gets.to_i
+# puts " "
+
+a = Rsa.new(m1)
+
+#Asignación de valores
+a.pe = pe
+a.q = q
+
+#Codificación del mensaje
+a.codificar(n)
+
+#Cálculo de fi
+a.calcular_fi(pe, q)
+puts a.fi
+
+#Cálculo de la e
+e = euclides_extendido(d, a.fi)
+if e < 0
+  e = (e + a.fi)
+end
 
 
+puts "MENU"
+puts "==========="
+puts ""
+puts "Elige una opción:
+1: Cifrar
+2: Descifrar"
+
+case gets.strip
+  when "1"
+    puts ""
+
+    a.cifrar(n, a.mensaje_codificado, e)
+
+    puts "Mensaje original: #{a.mensaje_original}"
+
+    puts "N: #{n}"
+    puts "Φ: #{a.fi}"
+    puts "e: #{e}"
+
+    puts "Mensaje cifrado: #{a.mensaje_cifrado. join(" ")}"
+  when "2"
+    puts ""
+    a.descifrar(n, a.mensaje_codificado, e)
+
+    puts "Mensaje cifrado: #{a.mensaje_original}"
+
+    puts "N: #{n}"
+    puts "Φ: #{a.fi}"
+    puts "e: #{e}"
+
+    puts "Mensaje descifrado: #{a.mensaje_cifrado. join(" ")}"
 end
